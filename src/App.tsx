f4357e6761285c9fbbd71cf4b2af46935b8c092e
Easy Code <easycode002@gmail.com>
2024-07-20 22:35:05 +0700

    modify in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainLayout from './components/organisms/layout/MainLayout'
 import MarqueeAnimationPage from './pages/animation-marquee/MarqueeAnimationPage'
+import SkeletonPage from './pages/SkeletonPage/SkeletonPage'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
         <Routes>
             <Route path='/' element={<MainLayout/>}>
                 <Route path='animation-marquee' element={<MarqueeAnimationPage/>}/>
+                <Route path='skeleton' element={<SkeletonPage/>}/>
             </Route>
         </Routes>
     </BrowserRouter>
